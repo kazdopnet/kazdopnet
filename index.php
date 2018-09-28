50c1cac8f787cb230112ad70d306c6939f17d7a7
kazdopnet <kazdopnet@gmail.com>
2018-09-28 10:12:27 +0000

    1234

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 
 require "application/lib/Dev.php";
-require "application/core/Router.php";
+
+use application\core\Router;
+use application\controllers\AccountController;
+
+spl_autoload_register(function($class){
+	$path = str_replace('\\', '/', $class.'.php');
+	if (file_exists($path) ) {
+		require $path;
+	}
+});
 
 session_start();
 
